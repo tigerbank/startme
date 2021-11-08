4540137c90b3670683c63ffd5cd24ad4419436cb
Teerasak <bank_hpk@hotmail.com>
2021-11-08 15:21:43 +0700

    Removed unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import FeedbackForm from './components/FeedbackForm';
 
