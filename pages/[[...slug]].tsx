354766dd6191baa036b4eeeded1b39404aaf6185
Teerasak <bank_hpk@hotmail.com>
2021-11-12 12:38:36 +0700

    Update slug page

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,6 +1,6 @@
 import { Box, Heading, Text } from '@chakra-ui/layout';
 import React from 'react';
-import { getPageData, getNavData } from 'util/api';
+import { getPageData, getNavData, fetchAPI } from 'util/api';
 
 function Page({ page }: any) {
   return (
@@ -11,7 +11,28 @@ function Page({ page }: any) {
   );
 }
 
-export async function getServerSideProps(context: any) {
+export async function getStaticPaths(context: any) {
+  // Get all pages from Strapi
+  const allPages = await context.locales.map(async (locale: any) => {
+    return fetchAPI(`/pages?_locale=${locale}`);
+  });
+
+  const pages = await Promise.all(allPages);
+
+  const pagesCombined = pages.flat();
+
+  const paths = pagesCombined.map((page: any) => {
+    const slugArray = !page.slug ? false : page.slug.split('/');
+
+    return {
+      params: { slug: slugArray },
+      locale: page.locale,
+    };
+  });
+
+  return { paths, fallback: 'blocking' };
+}
+export async function getStaticProps(context: any) {
   const { slug } = context.params;
   const { locale } = context;
 
--- util/api.tsx
+++ util/api.tsx
@@ -2,17 +2,21 @@ export function getStrapiURL(path: string) {
   return `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${path}`;
 }
 
-export async function fetchApi(path: string) {
+export async function fetchAPI(path: string) {
   const res = await fetch(getStrapiURL(path));
   return res.json();
 }
 
 export async function getPageData(slug: any, locale: string) {
   const joinedSlug = slug.join('/');
-  const data = await fetchApi(`/pages?slug=${joinedSlug}&_locale=${locale}`);
+  const data = await fetchAPI(`/pages?slug=${joinedSlug}&_locale=${locale}`);
+
+  if (data == null || data.length === 0) {
+    return null;
+  }
   return data[0];
 }
 
 export async function getNavData(locale: string) {
-  return fetchApi(`/navigations?_locale=${locale}`);
+  return fetchAPI(`/navigations?_locale=${locale}`);
 }
