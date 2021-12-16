418af9c5554b983a15d50e4ae8a75fde709ef4a4
Teerasak <bank_hpk@hotmail.com>
2021-12-17 02:17:28 +0700

    Remove appWithTranslation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import NextNprogress from 'nextjs-progressbar';
 import Layout from '@/components/Layout';
@@ -46,4 +45,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default appWithTranslation(MyApp);
+export default MyApp;
