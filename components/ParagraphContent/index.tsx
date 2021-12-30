16854b82797df4b4562a41d91146eb7ca383803d
Teerasak <bank_hpk@hotmail.com>
2021-12-31 01:27:05 +0700

    Update index.tsx

--- components/ParagraphContent/index.tsx
+++ components/ParagraphContent/index.tsx
@@ -11,9 +11,9 @@ function ParagraphContent() {
         <Box maxWidth="900px" ml="auto" mr="auto">
           <Text px="20px" mt="20px" textAlign="center">
             This site is a simple portfolio that contains multiple projects for
-            website development showcase and experiment. This website is built
-            with Nextjs, Strapi, Typescript, Chakra-ui, and many other
-            technologies. More detail about this project, <br />
+            website development showcase and experiment built with Nextjs,
+            Strapi, Typescript, Chakra-ui, and many other technologies. More
+            detail about this project, <br />
             <Text as="span" whiteSpace="nowrap">
               please check :
             </Text>
