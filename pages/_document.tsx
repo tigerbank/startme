4e343b1f5c9e5bd82629829c47ff94b7c1211bb3
Teerasak <bank_hpk@hotmail.com>
2022-01-04 21:14:19 +0700

    Add slick-theme.css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,11 +19,6 @@ class MyDocument extends Document {
             charSet="UTF-8"
             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
           />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
         </Head>
         <body>
           <Main />
