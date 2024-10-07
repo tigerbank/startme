8872bf6424a0845dea332037fc17bfd31f7069b4
tigerbank <bank_hpk@hotmail.com>
2024-10-07 15:38:59 +0700

    Removed

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Box, useBreakpointValue } from '@chakra-ui/react';
-import Header from './Header';
-import DesktopNavigation from './Navigation/Desktop';
-import { NavProps } from '@/interfaces/common';
-import Footer from '@/components/Layout/Footer';
+import React from "react"
+import { Box, useBreakpointValue } from "@chakra-ui/react"
+import Header from "./Header"
+import DesktopNavigation from "./Navigation/Desktop"
+import { NavProps } from "@/interfaces/common"
+import Footer from "@/components/Layout/Footer"
 
 function Layout({
   children,
   nav,
 }: {
-  children: React.ReactNode;
-  nav: NavProps[];
+  children: React.ReactNode
+  nav: NavProps[]
 }) {
-  const isMobile = useBreakpointValue({ base: true, md: false });
+  const isMobile = useBreakpointValue({ base: true, md: false })
 
   return (
     <>
       <Box h="100vh" d="flex" flexDir="column" justifyContent="space-between">
-        <Header nav={nav} />
+        {/* <Header nav={nav} />
         {!isMobile && nav && <DesktopNavigation nav={nav} />}
         <Box mb="40px" mt={{ base: '45px', md: '0' }}>
           {children}
         </Box>
-        <Footer />
+        <Footer /> */}
+        <div className="flex justify-center items-center h-screen">
+          "Be yourself; everyone else is already taken." — Oscar Wilde
+        </div>
       </Box>
     </>
-  );
+  )
 }
 
-export default Layout;
+export default Layout
--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,73 +1,73 @@
-import React from 'react';
-import { NextSeo } from 'next-seo';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Sections from '@/components/sections';
-import { PageProps } from '@/interfaces/common';
-import { getPageData, getGlobalData, fetchAPI } from '@/util/api';
-import Newsletter from '@/components/Newsletter';
-import ParagraphContent from '@/components/ParagraphContent';
+import React from "react"
+import { NextSeo } from "next-seo"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import Sections from "@/components/sections"
+import { PageProps } from "@/interfaces/common"
+import { getPageData, getGlobalData, fetchAPI } from "@/util/api"
+import Newsletter from "@/components/Newsletter"
+import ParagraphContent from "@/components/ParagraphContent"
 
 function Page({ page }: { page: PageProps }) {
-  const sections = page.contentSections;
+  const sections = page.contentSections
   return (
     <>
       <NextSeo
-        title={page.meta.metaTitle ? page.meta.metaTitle : ''}
-        description={page.meta.metaTitle ? page.meta.metaDescription : ''}
+        title={page.meta.metaTitle ? page.meta.metaTitle : ""}
+        description={page.meta.metaTitle ? page.meta.metaDescription : ""}
       />
-      <Sections sections={sections} />
-      <ParagraphContent />
+      {/* <Sections sections={sections} />
+      <ParagraphContent /> */}
       {/* <Newsletter /> */}
     </>
-  );
+  )
 }
 
 export async function getStaticPaths({ locales }: { locales: string[] }) {
   // Get all pages from Strapi
   const allPages = locales.map(async (locale: string) => {
-    return fetchAPI(`/pages?_locale=${locale}`, null);
-  });
+    return fetchAPI(`/pages?_locale=${locale}`, null)
+  })
 
-  const pages = await Promise.all(allPages);
+  const pages = await Promise.all(allPages)
 
-  const pagesCombined = pages.flat();
+  const pagesCombined = pages.flat()
 
   const paths = pagesCombined.map((page: any) => {
-    const slugArray = !page.slug ? [''] : page.slug.split('/');
+    const slugArray = !page.slug ? [""] : page.slug.split("/")
 
     return {
       params: { slug: slugArray },
       locale: page.locale,
-    };
-  });
+    }
+  })
 
-  return { paths, fallback: 'blocking' };
+  return { paths, fallback: "blocking" }
 }
 export async function getStaticProps({
   params,
   locale,
 }: {
-  params: { slug: string[] };
-  locale: string;
+  params: { slug: string[] }
+  locale: string
 }) {
-  const { slug } = params;
-  const page = await getPageData(slug ? slug : [''], locale);
-  const global = await getGlobalData(locale);
+  const { slug } = params
+  const page = await getPageData(slug ? slug : [""], locale)
+  const global = await getGlobalData(locale)
 
   if (!page) {
     return {
       notFound: true,
-    };
+    }
   }
 
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale, ["common"])),
       page,
       global,
     },
     revalidate: 10,
-  };
+  }
 }
 
-export default Page;
+export default Page
